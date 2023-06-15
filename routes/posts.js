4108d4e4db5ba14536f40fbe4b3716a25505c74d
leeseungjun <cjstkrak@gmail.com>
2023-06-16 02:37:15 +0900

    최종기능구현

--- routes/posts.js
+++ routes/posts.js
@@ -18,20 +18,21 @@ router.get("/posts", async (req, res) => {
   res.json({ posts: result });
 });
 
-router.get("/posts/:_Id", async (req, res) => {
+router.get("/posts/:postId", async (req, res) => {
   // 게시물 상세 조회
-  const { _Id } = req.params;
+  const { postId } = req.params;
   const posts = await Posts.find();
 
-  // let result = null;
-
-  // for (const good of goods) {
-  //   if (Number(goodsId) === good.goodsId) {
-  //     result = good;
-  //   }
-  // }
-
-  result = posts.filter((post) => _Id === post.id);
+  getPost = posts.filter((post) => postId === post.id);
+  result = getPost.map((post) => {
+    return {
+      _id: post.id,
+      user: post.user,
+      title: post.title,
+      content: post.content,
+      createdAt: post.createdAt,
+    };
+  });
   res.status(200).json({ detail: result });
 });
 
@@ -48,16 +49,16 @@ router.post("/posts", async (req, res) => {
   res.json({ message: "성공적으로 저장되었습니다." });
 });
 
-router.put("/posts/:_Id", async (req, res) => {
+router.put("/posts/:postId", async (req, res) => {
   // 게시물 수정
-  const { _Id } = req.params;
-  const { user, password, title, content, createdAt } = req.body;
+  const { postId } = req.params;
+  const { user, password, title, content } = req.body;
 
-  const existsPosts = await Posts.find({ _id: _Id });
+  const existsPosts = await Posts.find({ postId: postId });
 
   if (existsPosts.length && password === existsPosts[0].password) {
     await Posts.updateOne(
-      { _id: _Id },
+      { _id: postId },
       {
         $set: {
           user: user,
@@ -77,15 +78,15 @@ router.put("/posts/:_Id", async (req, res) => {
   });
 });
 
-router.delete("/posts/:_Id", async (req, res) => {
+router.delete("/posts/:postId", async (req, res) => {
   // 게시물 삭제
-  const { _Id } = req.params;
+  const { postId } = req.params;
   const { password } = req.body;
 
-  const existsPosts = await Posts.find({ _id: _Id });
+  const existsPosts = await Posts.find({ _id: postId });
 
   if (existsPosts.length && password === existsPosts[0].password) {
-    await Posts.deleteOne({ _id: _Id });
+    await Posts.deleteOne({ _id: postId });
     res.status(200).json({
       status: "success",
     });
@@ -96,16 +97,16 @@ router.delete("/posts/:_Id", async (req, res) => {
   }
 });
 
-router.post("/posts/:_Id/comments", async (req, res) => {
-  const { _Id } = req.params;
-  const existsPosts = await Posts.find({ _id: _Id });
+router.post("/posts/:postId/comments", async (req, res) => {
+  const { postId } = req.params;
+  const existsPosts = await Posts.find({ _id: postId });
   const { user, password, content } = req.body;
   if (existsPosts.length) {
     const createComments = await Comments.create({
       user,
       password,
       content,
-      postId: _Id,
+      postId: postId,
     });
 
     res.json({ message: "성공적으로 저장되었습니다." });
@@ -114,40 +115,87 @@ router.post("/posts/:_Id/comments", async (req, res) => {
   res.status(400).json({ message: "post가 존재하지 않습니다" });
 });
 
-router.get("/posts/:_Id/comments", async (req, res) => {
-  const { _Id } = req.params;
+router.get("/posts/:postId/comments", async (req, res) => {
+  const { postId } = req.params;
   const comments = await Comments.find();
-  result = comments.filter((comment) => _Id === comment.postId);
+  const getComments = comments.filter((comment) => postId === comment.postId);
+  result = getComments.map((comment) => {
+    return {
+      _id: comment._id,
+      user: comment.user,
+      content: comment.content,
+      postId: postId,
+      createdAt: comment.createdAt,
+    };
+  });
   res.status(200).json({ comments: result });
 });
 
-// router.put("/posts/:_Id", async (req, res) => {
-//   // 게시물 수정
-//   const { _Id } = req.params;
-//   const { user, password, title, content, createdAt } = req.body;
+router.get("/posts/:postId/comments/:commentId", async (req, res) => {
+  const { postId } = req.params;
+  const { commentId } = req.params;
+  const comments = await Comments.find();
+  const getComments = comments.filter(
+    (comment) => comment._id.toString() === commentId
+  );
+  result = getComments.map((comment) => {
+    return {
+      _id: comment._id,
+      user: comment.user,
+      content: comment.content,
+      postId: postId,
+      createdAt: comment.createdAt,
+    };
+  });
+  res.status(200).json({ comments: result });
+});
 
-//   const existsPosts = await Posts.find({ _id: _Id });
+router.put("/posts/:postId/comments/:commentId", async (req, res) => {
+  // 게시물 수정
+  const { commentId } = req.params;
+  const { user, password, content } = req.body;
 
-//   if (existsPosts.length && password === existsPosts[0].password) {
-//     await Posts.updateOne(
-//       { _id: _Id },
-//       {
-//         $set: {
-//           user: user,
-//           password: password,
-//           title: title,
-//           content: content,
-//           createdAt: Date(),
-//         },
-//       }
-//     );
-//     res.status(200).json({
-//       success: true,
-//     });
-//   }
-//   res.status(400).json({
-//     message: "존재하지 않거나 비밀번호가 맞지 않습니다",
-//   });
-// });
+  const existComments = await Comments.find({ _id: commentId });
+  console.log(existComments)
+
+  if (existComments.length && password === existComments[0].password) {
+    await Comments.updateOne(
+      { _id: commentId },
+      {
+        $set: {
+          user,
+          password,
+          content,
+          createdAt: Date(),
+        },
+      }
+    );
+    res.status(200).json({
+      success: true,
+    });
+  }
+  res.status(400).json({
+    message: "존재하지 않거나 비밀번호가 맞지 않습니다",
+  });
+});
+
+router.delete("/posts/:postId/comments/:commentId", async (req, res) => {
+  // 게시물 삭제
+  const { commentId } = req.params;
+  const { password } = req.body;
+
+  const existComments = await Comments.find({ _id: commentId });
+
+  if (existComments.length && password === existComments[0].password) {
+    await Comments.deleteOne({ _id: commentId });
+    res.status(200).json({
+      status: "success",
+    });
+  } else {
+    res.status(400).json({
+      status: "존재하지 않거나 비밀번호가 맞지 않습니다",
+    });
+  }
+});
 
 module.exports = router;
