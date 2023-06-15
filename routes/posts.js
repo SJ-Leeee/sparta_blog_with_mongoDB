afca800618c4460442d4c5759b1ada898baff0c7
leeseungjun <cjstkrak@gmail.com>
2023-06-15 20:56:49 +0900

    put에서 pssword 유효성검사

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const Posts = require("../schemas/posts.js");
 
-router.get("/posts", async (req, res) => { // 게시물 조회
+router.get("/posts", async (req, res) => {
+  // 게시물 조회
   const posts = await Posts.find();
   result = posts.map((post) => {
     return {
@@ -16,7 +17,8 @@ router.get("/posts", async (req, res) => { // 게시물 조회
   res.json({ posts: result });
 });
 
-router.post("/posts", async (req, res) => { // 게시물 등록
+router.post("/posts", async (req, res) => {
+  // 게시물 등록
   const { user, password, title, content } = req.body;
 
   const createPosts = await Posts.create({
@@ -29,7 +31,8 @@ router.post("/posts", async (req, res) => { // 게시물 등록
   res.json({ message: "성공적으로 저장되었습니다." });
 });
 
-router.get("/posts/:_Id", async (req, res) => { // 게시물 상세 조회
+router.get("/posts/:_Id", async (req, res) => {
+  // 게시물 상세 조회
   const { _Id } = req.params;
   const posts = await Posts.find();
 
@@ -45,14 +48,14 @@ router.get("/posts/:_Id", async (req, res) => { // 게시물 상세 조회
   res.status(200).json({ detail: result });
 });
 
-router.put("/posts/:_Id", async (req, res) => { // 게시물 수정
+router.put("/posts/:_Id", async (req, res) => {
+  // 게시물 수정
   const { _Id } = req.params;
   const { user, password, title, content } = req.body;
 
-
-  // const existsPosts = Posts.find((post) => post.id === _Id);// 왜안되지 ??
   const existsPosts = await Posts.find({ _id: _Id });
-  if (existsPosts.length) {
+
+  if (existsPosts.length && password === existsPosts[0].password) {
     await Posts.updateOne(
       { _id: _Id },
       {
@@ -64,19 +67,23 @@ router.put("/posts/:_Id", async (req, res) => { // 게시물 수정
         },
       }
     );
+    res.status(200).json({
+      success: true,
+    });
   }
-  res.status(200).json({
-    success: true,
+  res.status(400).json({
+    message: "존재하지 않거나 비밀번호가 맞지 않습니다",
   });
 });
 
-router.delete("/posts/:_Id", async (req, res) =>  { // 게시물 삭제
+router.delete("/posts/:_Id", async (req, res) => {
+  // 게시물 삭제
   const { _Id } = req.params;
 
   const existsPosts = await Posts.find({ _id: _Id });
 
   if (existsPosts.length) {
-    await Posts.deleteOne({ _id:_Id });
+    await Posts.deleteOne({ _id: _Id });
     res.status(200).json({
       status: "success",
     });
